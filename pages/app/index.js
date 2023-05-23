98bb4360f54554767fb08dabf10054549adb81c9
Carlos Castillo <carlos.castilloperera@gmail.com>
2023-05-23 06:29:48 -0600

    Update index.js

--- pages/app/index.js
+++ pages/app/index.js
@@ -38,10 +38,7 @@ export default function Home() {
         const file = await toBase64(event.target.files[0]);
         setBase64(file);
       } else
-        setIdea({
-          ...idea,
-          [event.target.name]: event.target.value.trim(),
-        });
+        setIdea({ ...idea, [event.target.name]: event.target.value.trim() });
     },
     [idea]
   );
